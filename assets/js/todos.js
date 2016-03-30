9471a6013f0637e0a51e8ab9c4b176013e8b994b
Brian <brian.lynnerup.pedersen@gmail.com>
2016-03-30 15:40:32 +0200

    Created logic for click events
    Created the functionalities for the clicking on either "li" or "span"
    elements. This includes toggeling a css class when clicking on the li
    and a fadeOut and remove functionality for the span.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,22 @@
-﻿alert("CONNECTED");
+﻿// Check off specific todos by clicking
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete ToDo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // Grab new todo text from input
+        var todoText = ($(this).val());
+        $(this).val("");
+        // Create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
